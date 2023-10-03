b3ccd799c60d1ade92d74ab12e044cf7e2a22891
KotovaTatiana <kotova6301@gmail.com>
2023-10-03 16:39:46 +0300

    Вычисление суммы 2х последних знаков числа

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,15 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите число:");
+        new Scanner(System.in);
+        System.out.println("Введите дробное число:");
         double x = new Scanner(System.in).nextDouble();
-
         System.out.println("Дробная часть: ");
         System.out.format("%.3f", fraction(x));
+
+        System.out.println("\nВведите целое число (количество знаков >= 2): ");
+        int sumLast = new Scanner(System.in).nextInt();
+        System.out.println("Сумма последних 2 знаков: " + sumLastNums(sumLast));
     }
 
     public static double fraction(double x) {
@@ -19,5 +23,11 @@ public class Main {
         return res2;
     }
 
+    public static int sumLastNums(int x) {
+        int num1 = x % 10;
+        int num2 = (x / 10) % 10;
+        int res = num1 + num2;
+        return res;
+    }
 
 }
