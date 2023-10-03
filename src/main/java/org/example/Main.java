d9666008389f1251f65c5b4afff718a2af091681
KotovaTatiana <kotova6301@gmail.com>
2023-10-03 17:14:06 +0300

    Вывод кода символа

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,25 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
+
+        //Задание 1
         System.out.println("Введите дробное число:");
-        double x = new Scanner(System.in).nextDouble();
+        double x = in.nextDouble();
         System.out.println("Дробная часть: ");
         System.out.format("%.3f", fraction(x));
 
+        //Задание 2
         System.out.println("\nВведите целое число (количество знаков >= 2): ");
-        int sumLast = new Scanner(System.in).nextInt();
+        int sumLast = in.nextInt();
         System.out.println("Сумма последних 2 знаков: " + sumLastNums(sumLast));
+
+        //Задание 3
+        System.out.println("\nВведите цифру (от 0 до 9): ");
+        String inputChar = in.nextLine();
+        char charIn = inputChar.charAt(0);
+        System.out.println("Код символа " + charIn + ": " + charToNum(charIn));
+
     }
 
     public static double fraction(double x) {
@@ -28,6 +38,10 @@ public class Main {
         int num2 = (x / 10) % 10;
         int res = num1 + num2;
         return res;
+    }
+
+    public static int charToNum(char x){
+        return  (int)x;
     }
 
 }
